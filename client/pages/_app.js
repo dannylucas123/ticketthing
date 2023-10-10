12ec3dcdecb1ee91b554be2a6ac16a63d15de081
Danny <danny.lucas@live.nl>
2023-10-10 23:04:06 +0200

    .

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
-const A = ({ Component, pageProps }) => <Component {...pageProps} />
+const A = ({ Component, pageProps, currentUser }) => (
+  <>
+    <h1>Hello</h1>
+    <Component {...pageProps} />
+  </>
+)
+
+A.getInitialProps = async({ ctx, Component }) => {
+  const client = buildClient(ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+
+  return { pageProps, currentUser: data.currentUser };
+}
 
 export default A;
